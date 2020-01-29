909b0e097ad198f1bf136210df6a3f58ac1f102c
KenNgo <phucuongngo@gmail.com>
2020-01-29 16:58:22 -0600

    add interface

---
+++ src/arc180006/BoundedQueueInterface.java
@@ -0,0 +1,53 @@
+package arc180006;
+
+/**
+ * Bounded queue interface using array
+ * @author cuong
+ *
+ * @param <T>
+ */
+public interface BoundedQueueInterface<T> {
+	
+	/**
+	 * add element x of type T add the rear of the queue
+	 * @param x
+	 * @return
+	 */
+	boolean offer(T x);
+	
+	/**
+	 * remove and return the element of type T at the front of the queue
+	 * @return
+	 */
+	T poll();
+	
+	/**
+	 * 
+	 * @return the front element of type T at the front of the queue
+	 */
+	T peek();
+	
+	/**
+	 *
+	 * @return the number of elements of the queue
+	 */
+	int size();
+	
+	/**
+	 * check if the queue is empty
+	 * @return true if the queue is empty otherwise false
+	 */
+	boolean isEmpty();
+	
+	/**
+	 * clear the queue and set size to 0
+	 */
+	void clear();
+	
+	/**
+	 * fill user supplied array with the elements of the queue,
+	 * in queue order
+	 * @param a - array of type T supplied by user
+	 */
+	void toArray(T[] a);
+}
