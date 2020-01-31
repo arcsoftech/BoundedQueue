eea8bfb96e442b1eec417acc98e4da96b493bfd5
KenNgo <phucuongngo@gmail.com>
2020-01-31 11:58:11 -0600

    test copy function

--- src/test/BoundedQueueTest.java
+++ src/test/BoundedQueueTest.java
@@ -45,19 +45,64 @@ class BoundedQueueTest {
 			assertFalse(queue.isEmpty());
 			assertEquals(1, queue.peek());
 		}
+		assertEquals(3, queue.size());
 //		queue.printQueue();
 		
 		assertEquals(1,queue.poll());
 		assertEquals(2,queue.poll());
 		assertTrue(queue.offer(4));
 		assertTrue(queue.offer(5));
+		assertEquals(3, queue.size());
 		queue.printQueue();
 		assertEquals(3,queue.poll());		
 		assertEquals(4,queue.poll());		
 		assertEquals(5,queue.poll());	
+		assertEquals(null, queue.peek());
+		assertEquals(null, queue.poll());
 		assertTrue(queue.isEmpty());
+		assertEquals(0, queue.size());
 		
 	}
+	
+	@Test
+	void testMultipleItemsQueue2() {
+		BoundedQueue<Integer> queue = new BoundedQueue<>(3);
+		assertTrue(queue.isEmpty());
+		for (int i = 1; i <= 3; i++) {
+			queue.offer(i);
+			assertFalse(queue.isEmpty());
+			assertEquals(1, queue.peek());
+		}
+		assertEquals(3, queue.size());
+		queue.clear(); queue.clear();
+		assertEquals(0, queue.size());
+		for (int i = 1; i <= 3; i++) {
+			queue.offer(i);
+			assertFalse(queue.isEmpty());
+			assertEquals(1, queue.peek());
+		}
+		assertEquals(3, queue.size());
+	}
+	
+	@Test
+	void testCopyArray() {
+		Integer[] array1 = new Integer[3];
+		BoundedQueue<Integer> queue = new BoundedQueue<>(3);
+		assertTrue(queue.isEmpty());
+		for (int i = 0; i <= 2; i++) {
+			queue.offer(i);
+			assertFalse(queue.isEmpty());
+			assertEquals(0, queue.peek());
+		}
+		
+		queue.toArray(array1);
+		for (int i = 0; i <= 2; i++) {
+			assertEquals(i, array1[i]);
+		}
+		
+	}
+	
+
 	
 
 }
