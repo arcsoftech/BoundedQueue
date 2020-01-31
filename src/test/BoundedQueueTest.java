2332d4eabb658f0775d01832ab26935fdec7bd6d
KenNgo <phucuongngo@gmail.com>
2020-01-31 12:02:52 -0600

    increase coverage

--- src/test/BoundedQueueTest.java
+++ src/test/BoundedQueueTest.java
@@ -45,9 +45,7 @@ class BoundedQueueTest {
 			assertFalse(queue.isEmpty());
 			assertEquals(1, queue.peek());
 		}
-		assertEquals(3, queue.size());
-//		queue.printQueue();
-		
+		assertEquals(3, queue.size());		
 		assertEquals(1,queue.poll());
 		assertEquals(2,queue.poll());
 		assertTrue(queue.offer(4));
@@ -99,10 +97,27 @@ class BoundedQueueTest {
 		for (int i = 0; i <= 2; i++) {
 			assertEquals(i, array1[i]);
 		}
-		
 	}
 	
-
-	
-
+	@Test
+	void testCopyArray2() {
+		Integer[] array1 = new Integer[3];
+		BoundedQueue<Integer> queue = new BoundedQueue<>(3);
+		assertTrue(queue.isEmpty());
+		for (int i = 0; i <= 2; i++) {
+			queue.offer(i);
+			assertFalse(queue.isEmpty());
+			assertEquals(0, queue.peek());
+		}
+		queue.poll();
+		queue.poll();
+		queue.offer(3);
+		queue.offer(4);
+		queue.offer(5);
+		
+		queue.toArray(array1);
+		for (int i = 0; i <= 2; i++) {
+			assertEquals(i+2, array1[i]);
+		}
+	}
 }
